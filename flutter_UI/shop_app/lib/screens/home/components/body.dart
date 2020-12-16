361aad8d231efc7bf8e9c9eb609648131655205e
LazyboyEmpires <nixsunsee@gmail.com>
2020-12-16 23:03:33 +0700

    init commit

--- flutter_UI/shop_app/lib/screens/home/components/body.dart
+++ flutter_UI/shop_app/lib/screens/home/components/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/Product.dart';
 
 import 'categories.dart';
 
@@ -20,9 +22,68 @@ class Body extends StatelessWidget {
           ),
         ),
         Categories(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPanddin),
+            child: GridView.builder(
+              itemCount: products.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: kDefaultPanddin,
+                crossAxisSpacing: kDefaultPanddin,
+                childAspectRatio: 0.75,
+              ),
+              itemBuilder: (context, index) => ItemCard(
+                product: products[index],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
 }
 
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final Function press;
 
+  const ItemCard({
+    Key key,
+    this.product,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+            child: Container(
+            padding: EdgeInsets.all(kDefaultPanddin),
+            //they just want to demo, we dont need them no more
+            // height: 100,
+            // width: 160,
+            decoration: BoxDecoration(
+              color: product.color,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Image.asset(product.image),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: kDefaultPanddin / 4),
+          child: Text(
+            product.title,
+            style: TextStyle(color: kTextLightColor),
+          ),
+        ),
+        Text(
+          "\$${product.price}",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+}
