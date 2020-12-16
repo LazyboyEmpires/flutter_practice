8440f5d57f75b073f4b01f54b48c34f6e77a92ce
LazyboyEmpires <nixsunsee@gmail.com>
2020-12-16 22:02:21 +0700

    init commit

--- flutter_UI/shop_app/lib/screens/home/components/body.dart
+++ flutter_UI/shop_app/lib/screens/home/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/constants.dart';
 
+import 'categories.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,45 +25,4 @@ class Body extends StatelessWidget {
   }
 }
 
-class Categories extends StatefulWidget {
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
 
-class _CategoriesState extends State<Categories> {
-  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
-  int selectedIndex = 0;
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 25,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (context, index) => buildCategory(index),
-      ),
-    );
-  }
-
-  Widget buildCategory(int index) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPanddin),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              categories[index],
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: selectedIndex == index ? kTextColor : kTextLightColor,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: kDefaultPanddin / 4),
-              height: 2,
-              width: 30,
-              color: selectedIndex == index ? Colors.black : Colors.transparent,
-            )
-          ],
-        ),
-      );
-}
