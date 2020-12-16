40d6e478413751172d7f88d1a817cb9a893ce5ff
LazyboyEmpires <nixsunsee@gmail.com>
2020-12-16 14:09:22 +0700

    init commit

--- flutter_UI/shop_app/lib/screens/home/components/body.dart
+++ flutter_UI/shop_app/lib/screens/home/components/body.dart
@@ -9,12 +9,45 @@ class Body extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: kDefaultPanddin),
           child: Text(
-            "Women", style: Theme.of(context).textTheme
-            .headline5
-            .copyWith(fontWeight: FontWeight.bold),
+            "Women",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(fontWeight: FontWeight.bold),
           ),
-        )
+        ),
+        Categories(),
       ],
-    )
+    );
   }
+}
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hand bag", "Jewellery", "Footwear", "Dresses"];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => buildCategory(index),
+      ),
+    );
+  }
+
+  Widget buildCategory(int index) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPanddin),
+        child: Text(
+          categories[index],
+          style: TextStyle(fontWeight: FontWeight.bold,
+          color: kTextColor),
+        ),
+      );
 }
